45917775dadb1be02f03a08028ac566521dbe79e
Akira Moroo <retrage01@gmail.com>
2022-06-04 18:26:40 +0900

    scripts: Cleanup gen_report.py
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- scripts/gen_report.py
+++ scripts/gen_report.py
@@ -4,7 +4,9 @@ import sys
 from typing import Final
 
 from parse import parse
-from junitparser import Error, JUnitXml, TestCase, TestSuite
+from junitparser import Attr, Error, JUnitXml, TestCase, TestSuite
+
+TestCase.file = Attr("file")
 
 def parse_logline(line):
   LOG_FORMAT: Final = "{date:ti} {time:tt} {log_level:5} ({module:w}) {message}  \n"
@@ -13,25 +15,47 @@ def parse_logline(line):
     return None
   return (result["date"], result["time"], result["log_level"], result["module"], result["message"])
 
-def create_testcase_from_log(loginfo):
+def create_smm_callout_testcase(binary_path, log_level, module, message):
   SMM_CALLOUT_LOG_FORMAT: Final = "Potential SMM callout detected at {func_name:w}({func_addr:x}) : {addr:x}"
-  SMM_GET_VARIABLE_OVERFLOW_LOG_FORMAT: Final = "Potential GetVariable overflow detected at {func_name:w}({func_addr:x}) : {addr1:x} and {addr2:x}"
-  _, _, log_level, module, message = loginfo
-  if module != "EfiSeek":
+  if module != "EfiSeek" or log_level != "WARN ":
     return None
-  if log_level == "WARN ":
-    result = parse(SMM_GET_VARIABLE_OVERFLOW_LOG_FORMAT, message)
-    if result is not None:
-      testcase = TestCase(name="Potential SMM GetVariable Overflow : {addr1:#x} and {addr2:#x}".format(addr1=result["addr1"], addr2=result["addr2"]), classname="{func_addr:#x}".format(func_addr=result["func_addr"]))
-      testcase.result = [Error()]
-      testcase.system_err = message
-      return testcase
-    result = parse(SMM_CALLOUT_LOG_FORMAT, message)
-    if result is not None:
-      testcase = TestCase(name="Potential SMM Callout : {addr:#x}".format(addr=result["addr"]), classname="{func_addr:#x}".format(func_addr=result["func_addr"]))
-      testcase.result = [Error()]
-      testcase.system_err = message
-      return testcase
+  result = parse(SMM_CALLOUT_LOG_FORMAT, message)
+  if result is not None:
+    addr = result["addr"]
+    func_name = result["func_name"]
+    func_addr = result["func_addr"]
+    testcase = TestCase(name="Potential SMM Callout : {:#x}".format(addr), classname="{}:{:#x}".format(func_name, func_addr))
+    testcase.result = [Error()]
+    testcase.file = binary_path
+    testcase.system_err = message
+    return testcase
+  return None
+
+def create_smm_get_variable_overflow_testcase(binary_path, log_level, module, message):
+  SMM_GET_VARIABLE_OVERFLOW_LOG_FORMAT: Final = "Potential GetVariable overflow detected at {func_name:w}({func_addr:x}) : {addr1:x} and {addr2:x}"
+  if module != "EfiSeek" or log_level != "WARN ":
+    return None
+  result = parse(SMM_GET_VARIABLE_OVERFLOW_LOG_FORMAT, message)
+  if result is not None:
+    addr1 = result["addr1"]
+    addr2 = result["addr2"]
+    func_name = result["func_name"]
+    func_addr = result["func_addr"]
+    testcase = TestCase(name="Potential SMM GetVariable Overflow : {:#x} and {:#x}".format(addr1, addr2), classname="{}:{:#x}".format(func_name, func_addr))
+    testcase.result = [Error()]
+    testcase.file = binary_path
+    testcase.system_err = message
+    return testcase
+  return None
+
+def get_binary_path(log_level, module, message):
+  HEADLESS_ANALYZER_INFO_FORMAT: Final = "ANALYZING all memory and code: {binary_path}" 
+  if module != "HeadlessAnalyzer" or log_level != "INFO ":
+    return None
+  print(message)
+  result = parse(HEADLESS_ANALYZER_INFO_FORMAT, message)
+  if result is not None:
+    return result["binary_path"]
   return None
 
 def main():
@@ -42,19 +66,31 @@ def main():
   input_file = sys.argv[1]
   output_file = sys.argv[2]
 
-  # TODO: Use binary name for test suite name
-  testsuite = TestSuite(name=input_file)
+  smm_callout_testsuite = TestSuite("SMM Callout")
+  smm_get_variable_overflow_testsuite = TestSuite("SMM GetVariable Overflow")
 
   with open(input_file, 'r') as f:
+    binary_path = ""
     for line in f:
       loginfo = parse_logline(line)
       if loginfo is None:
         continue
-      testcase = create_testcase_from_log(loginfo)
+      _, _, log_level, module, message = loginfo
+      path = get_binary_path(log_level, module, message)
+      if path is not None:
+        binary_path = path
+        continue
+      testcase = create_smm_callout_testcase(binary_path, log_level, module, message)
       if testcase is not None:
-        testsuite.add_testcase(testcase)
-  xml= JUnitXml("Ghidra efiSeek Static SMM Analysis")
-  xml.add_testsuite(testsuite)
+        smm_callout_testsuite.add_testcase(testcase)
+        continue
+      testcase = create_smm_get_variable_overflow_testcase(binary_path, log_level, module, message)
+      if testcase is not None:
+        smm_get_variable_overflow_testsuite.add_testcase(testcase)
+        continue
+  xml= JUnitXml("Ghidra efiSeek Static Analysis")
+  xml.add_testsuite(smm_callout_testsuite)
+  xml.add_testsuite(smm_get_variable_overflow_testsuite)
   xml.write(output_file)
 
   return 0
